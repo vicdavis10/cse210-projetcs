05b50e879e00623c617192f15ad04176ea42eb15
vicdavis10 <davisvictoria2015@hotmail.com>
2025-03-06 15:26:08 -0600

    W01 Assignment Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Net;
 
-class Program
+Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+bool playAgain = true; // a boolean variable to control the game loop
+
+while (playAgain)
 {
-    static void Main(string[] args)
+    Random randomGenerator = new Random();
+    int magicNumber = randomGenerator.Next(1, 100);
+    int guessNumber = 0;
+    int numberOfGuesses = 0;
+
+    do
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is your guess?");
+        string guess = Console.ReadLine();
+        guessNumber = int.Parse(guess);
+        numberOfGuesses++;
+
+
+        if (magicNumber < guessNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (magicNumber > guessNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You took {numberOfGuesses} guesses");
+        }
+
+    } while (guessNumber != magicNumber);
+
+    Console.Write("Do you want to play again? (yes or no)");
+    string playAgainAnswer = Console.ReadLine();
+    if (playAgainAnswer != "yes")
+    {
+        playAgain = false; // set playAgain to false to exit the game loop
     }
 }
