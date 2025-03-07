d39ccb879907d7c3e1d5b1f6a343089df0c6d841
vicdavis10 <davisvictoria2015@hotmail.com>
2025-03-06 21:46:23 -0600

    W01 Assignment: Exercise 5: Functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Reflection.Metadata;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    
+    //Remember you need to declare the function before you can call it
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int result = SquareNumber(userNumber);
+
+        DisplayResult(userName , result);
     }
+
+    static void DisplayWelcome ()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }    
+
+    static string PromptUserName ()
+    {   
+      Console.Write("Please enter your name:");
+      return Console.ReadLine(); 
+    }
+    //  Call the function and store the result in a variable
+
+    static int PromptUserNumber ()
+    {
+        Console.Write("Please enter your favorite number:");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+    static int SquareNumber(int userNumber)
+    { 
+        int result = userNumber * userNumber;
+        return result;
+    }
+
+    static void DisplayResult (string userName, int result)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {result}");
+    }
+    
 }
